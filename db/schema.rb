b9c877f50a98eabaaab349b1324d9f5d67622c54
Victor Man <woodbj@yahoo.com>
2026-01-31 16:26:20 -0500

    Track clients table migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_30_232954) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_31_211949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -20,4 +30,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_30_232954) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "clients", "users"
 end
